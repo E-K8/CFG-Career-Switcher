81b7e190b54a4731f7ae9b5b4a930cfafdb9a89b
E-K8 <ekatplat@gmail.com>
2021-10-18 15:21:11 +0100

    basic option of the game at this point

--- homework/week4/assets/scripts/app.js
+++ homework/week4/assets/scripts/app.js
@@ -1,13 +1,28 @@
-// DATA TYPES 
+const options = [
+    `Highly likely.`,
+    `I can't see it happening soon, but it's possible.`,
+    `Of course, darling! No doubt about that.`,
+    `No. It can become 'yes' if your want it badly enough.`,
+    `The answer isn't clear. Are you sure you wanted to ask that?`,
+    `Ask yourself - deep down you already know the answer.`,
+    `It will happen for you in the most magical way.`,
+    `Unclear. Please reformulate.`,
+    `The autumn will be wonderful for you, but the winter will be even better.`
+];
 
-// DATA TYPES EXERCISE
 
-// IF/ELSE STATEMENTS 
+function randomReply() {
+    // generate a random number between 0 and the number of the array items
+    const randomNumber = Math.floor(Math.random() * options.length);
 
-// IF/ELSE EXERCISE
+    // print the random number to the console log to make sure it's working
+    console.log("random number: " + randomNumber);
 
-// ARRAY 
+    // take the fortune at the random spot in the array and insert it into the "fortune" div
+    document.getElementById("reply1").innerHTML = options[randomNumber];
+}
 
-// ARRAY EXERCISE
-
-// FINAL CLASS EXERCISE
+function reset() {
+    // what goes here? (hint: make the fortune div contain an empty string)
+    document.getElementById("reply1").innerHTML = "";
+}
